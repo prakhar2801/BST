91ff7deb5a0730af976e8e873c5af0327418499d
Prateek Kr Singh <44129531+NoOne03@users.noreply.github.com>
2020-10-02 00:57:49 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,10 +1,13 @@
+//Import libraries
 #include<bits/stdc++.h>
 using namespace std;
+//Define a node
 struct node
 {
 	int key;
 	struct node *left,*right;
 };
+//call this function to alocate new node
 struct node* newNode(int item) 
 { 
     struct node *temp =new node; 
@@ -12,6 +15,7 @@ struct node* newNode(int item)
     temp->left = temp->right = NULL; 
     return temp; 
 }
+//To insert a new node in bst
 struct node* insert(struct node* node, int key) 
 { 
     if (node == NULL) 
@@ -24,6 +28,7 @@ struct node* insert(struct node* node, int key)
   
     return node; 
 } 
+//Inorder traversal
 void inorder(struct node* root)
 {
 	if(root!=NULL)
@@ -33,6 +38,7 @@ void inorder(struct node* root)
 		inorder(root->right);
 	}
 }
+//Pre-order traversal
 void preorder(struct node* root)
 {
 	if(root!=NULL)
@@ -43,6 +49,7 @@ void preorder(struct node* root)
 		preorder(root->right);
 	}
 }
+//Post-order traversal
 void postorder(struct node* root)
 {
 	if(root!=NULL)
@@ -53,7 +60,7 @@ void postorder(struct node* root)
 		cout<<root->key<<"->";
 	}
 }
-
+//Return minimum from bst
 struct node* minValueNode(struct node* node) 
 { 
     struct node* current = node; 
@@ -64,7 +71,7 @@ struct node* minValueNode(struct node* node)
   
     return current; 
 } 
-  
+ //Delete the node having given key
 struct node* deleteNode(struct node* root, int key) 
 { 
     if (root == NULL) return root; 
@@ -104,8 +111,8 @@ int main()
 	root=insert(root,1);
 	for (int i=2;i<10;i++)
 	insert(root,i);
-	
-	
+	root=deleteNode(root,4);   //Delete a node 
+	cout<<minValueNode<<endl; //Check min Function
 	inorder(root);
 	cout<<"\n";
 	
